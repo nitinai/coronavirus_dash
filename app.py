5b17c8cf64d1f3f8409d9ef9b2a6fe5403a4b874
Patil, Nitin (PrdEng) <nitin_patil@siemens.com>
2020-04-05 23:12:51 +0530

    data update

--- app.py
+++ app.py
@@ -236,6 +236,38 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets,
 
 app.title = TITLE
 
+app.index_string = """<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MQ9HJRF');</script>
+        <!-- End Google Tag Manager -->
+        
+        {%metas%}
+        <title>{%title%}</title>
+        <!-- {%favicon%} -->
+        {%css%}
+        <script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=5e8a16e50febbf0019e83180&product=sticky-share-buttons&cms=sop' async='async'></script>
+    </head>
+    <body>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MQ9HJRF"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>"""
+
 app.config['suppress_callback_exceptions'] = True
 
 server = app.server # the Flask app to run it on web server
