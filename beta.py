6e3f97c4ae053b9657add5b2715692731bc1cbb7
Patil, Nitin (PrdEng) <nitin_patil@siemens.com>
2020-06-05 12:54:26 +0530

    data update

--- beta.py
+++ beta.py
@@ -14,8 +14,9 @@ from dash.dependencies import Input, Output
 from dash_table import DataTable
 #from dash_table.Format import Sign
 import dash_table.FormatTemplate as FormatTemplate
+import dash_bootstrap_components as dbc
 
-#import plotly.express as px
+import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 #from model import graph_scatter_mapbox, load_time_series_data, relative_trend_graph_china_vs_world, get_country_trend
@@ -235,7 +236,7 @@ def apply_line_plot_layout(fig, country, annot, annot_size=60, y_title=""):
         #showlegend=False,
         legend_orientation="h",
         legend=dict(x=0.02, y=1.08, bgcolor="rgba(0,0,0,0)",),
-        hovermode='x',
+        hovermode='x unified',
         #title="Daily Trend",
         xaxis= dict(fixedrange = True, # Disable zoom
                     #tickangle=-45,
