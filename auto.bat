52a4941266ede38995303c431759d1e771e1b79c
Nitin Patil <patilnitin23@gmail.com>
2020-09-12 11:03:19 +0530

    data update

--- auto.bat
+++ auto.bat
@@ -1,5 +1,7 @@
 ECHO ON
 echo Running auto script
+d:
+cd D:\workdir\ML\ml_units\kaggle\Vis\coronavirus\Dashboard\coronavirus_dash
 cd data_sources\COVID-19
 git pull
 cd ..\..
@@ -8,5 +10,5 @@ python utils_update_data.py --p True
 start heroku local web -f Procfile.Windows
 timeout 6
 start "" http://127.0.0.1:5000/
-REM push.bat
+push.bat
 PAUSE
